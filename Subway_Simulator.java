e2bf4397871e18d6d90174b49b56a087e8432784
Cyber-Spectre <52917568+Cyber-Spectre@users.noreply.github.com>
2021-05-28 11:16:47 -0600

    Add files via upload

--- Subway_Simulator.java
+++ Subway_Simulator.java
@@ -30,6 +30,10 @@ public class Subway_Simulator {
         int drink = rand.nextInt(75);
         int supp = rand.nextInt(75);
         
+        //Day variables
+        int day = 1;
+        int totalDays = 14;
+        
         /*
         Currency
         starts at random number between $400 and $1000
@@ -64,18 +68,35 @@ public class Subway_Simulator {
         //Misc variables
         int menuChoice, quantity;
         boolean playAgain = true;
-        // /Variables
+        
+        //Stat variables
+        double totalSpent = 0.00;
+        double totalEarned = 0.00;
+        int totalCustomers = 0;
+        int totalResourcesBought = 0;
+        int totalResourcesUsed = 0;
+        int totalServedCorrect = 0;
+        int totalServedIncorrect = 0;
+        int totalUnserved = 0;
+        int currentLoyal = 0;
+        int loyalRecieved = 0;
+        int loyalLost = 0;
         
         //Game code
+        //Intro
+        System.out.println("Welcome to Subway Simulator!\n"
+            + "==============================\n"
+            + "Your manager is sick and will need 2 weeks to recover, he made you the substitute manager while he is gone, maybe if you do good enough, you'll get a promotion! \n"
+            + "==============================\n"
+            + "\n");
         do{
             
             //Start
-            System.out.println("Welcome to Subway Simulator!\n"
+            int daysLeft = totalDays - day + 1;
+            System.out.println("Day " + day + " out of " + totalDays + ".\n"
+                + "You have " + daysLeft + " days until the manager is better.\n"
                 + "==============================\n"
-                + "Your manager is sick and will need 2 weeks to recover, he made you the substitute manager while he is gone, maybe if you do good enough, you'll get a promotion! \n"
-                + "\n"
-                + "==============================\n"
-                + "You are starting with the following:\n"
+                + "You currently have:\n"
                 + bread + " bread\n"
                 + meat + " meat\n"
                 + cheese + " cheese\n"
@@ -85,12 +106,48 @@ public class Subway_Simulator {
                 + supp + " supplies\n"
                 + "and $" + money + "\n"
                 + "==============================\n"
-                        + "What would you like to do? (Make sure you buy enough resources)\n"
-                        + "(0) Open the restaurant\n"
-                        + "(1) Order more resources\n"
-                        + "(2) Exit the game");
-           
+                + "What would you like to do?\n"
+                + "(0) Open the restaurant\n"
+                + "(1) Order more resources\n"
+                + "(2) Check game stats\n"
+                + "(3) Exit the game\n"
+                + "==============================");
             
+            menuChoice = scan1.nextInt();
+            
+            switch (menuChoice) {
+                
+                case 0: //Open restaurant
+                    
+                    break;
+                    
+                case 1: //Order resources
+                    
+                    break;
+                    
+                case 2: //Game stats
+                    System.out.println("Welcome to the stats menu!\n"
+                        + "==============================\n"
+                        + "You have spent: $" + totalSpent + "\n"
+                        + "You have earned: $" + totalEarned + "\n"
+                        + "You have bought " + totalResourcesBought + " resources\n"
+                        + "You have used " + totalResourcesUsed + " resources\n"
+                        + "You've had a total of " + totalCustomers + " customers;\n"
+                        + "---- " + totalServedCorrect + " served correctly\n"
+                        + "---- " + totalServedIncorrect + " served incorrectly\n"
+                        + "---- " + totalUnserved + " have left the restaurant unserved\n"
+                        + "You currently have " + currentLoyal + " loyal customers;\n"
+                        + "---- " + loyalRecieved + " Have been recieved\n"
+                        + "---- " + loyalLost + " Have been lost\n"
+                        + "");
+                    break;
+                    
+                case 3: //Exit game
+                    System.out.println("Thank you for playing, goodbye!");
+                    playAgain = false;
+                    break;
+                
+            }
             
         }
         while (playAgain);
